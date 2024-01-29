576dcc6716ef47dbe7f9ab73c8ef68556eece0b3
Vicky AS <vickyhuang808@gmail.com>
2024-01-29 19:56:49 +0700

    conditional rendering

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -2,10 +2,12 @@
 eslint-disable react/prop-types
 */
 
+import { Link } from "react-router-dom";
+
 
 const AuthLayout = (props) => {
 
-    const {children,title} = props;
+    const {children,title,type} = props;
     return (
         <div className="flex justify-center min-h-screen items-center">
         <div className="w-full max-w-xs">
@@ -14,6 +16,20 @@ const AuthLayout = (props) => {
                 Welcome, please enter your user details
             </p>
             {children}
+            <p className="text-sm mt-5 text-center">
+                {type == 'login'? "Don't have an account? ": "Already have an account? "}
+
+                {type == 'login'&&
+                <Link to="/register" className="font-bold text-blue-600">
+                Register
+                </Link> 
+                }
+                {type == 'register'&&
+                <Link to="/login" className="font-bold text-blue-600">
+                Login
+                </Link> 
+                }
+            </p>
         </div>
         </div>
     );
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,16 +1,10 @@
-import { Link } from "react-router-dom";
 import FormLogin from "../components/Fragments/formLogin";
 import AuthLayout from "../components/Layouts/AuthLayouts";
 const LoginPage = () => {
     return (
-        <AuthLayout title="Login">
+        <AuthLayout title="Login" type="login">
             <FormLogin />
-            <p className="text-sm mt-5 text-center">
-                Don&apos;t have an account?{" "}
-                <Link to="/register" className="font-bold text-blue-600">
-                Register
-                </Link> 
-            </p>
+            
         </AuthLayout>
     );
 }

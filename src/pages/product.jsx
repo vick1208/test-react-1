237a848a0e25345529f90ba079789ce4767b1922
Vicky AS <vickyhuang808@gmail.com>
2024-03-09 22:31:02 +0700

    rendering list

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,24 +1,45 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
 
-const ProductPage = () => {
 
+const products = [
+    {
+        id: 1,
+        name: "Kaos Baru",
+        price: "Rp. 302.000",
+        image: "/img/black-tee.jpg",
+        description: `Lorem ipsum dolor 
+        sit amet consectetur adipisicing elit. 
+        Molestias perspiciatis quae deleniti saepe quod quas, porro expedita 
+        beatae est cumque ad sequi nesciunt ipsam 
+        fuga molestiae ratione asperiores voluptates ipsa.`
+    },
+    {
+        id: 2,
+        name: "Kaos Lama",
+        price: "Rp. 302.000",
+        image: "/img/black-tee.jpg",
+        description: `Lorem ipsum dolor 
+        sit amet consectetur adipisicing elit. 
+        `
+    },
+];
+
+
+function ProductPage() {
     return (
-        <div className="flex justify-center py-4">
-            <CardProduct>
-                <CardProduct.Header image="/img/black-tee.jpg" />
-                <CardProduct.Body title="Kaos baru">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias perspiciatis quae deleniti saepe quod quas, porro expedita beatae est cumque ad sequi nesciunt ipsam fuga molestiae ratione asperiores voluptates ipsa.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp. 302.000" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="/img/black-tee.jpg" />
-                <CardProduct.Body title="Kaos baru">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias perspiciatis quae deleniti saepe quod quas, porro expedita beatae est cumque ad sequi nesciunt ipsam fuga molestiae ratione asperiores voluptates ipsa.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp. 302.000" />
-            </CardProduct>
+        <div className="flex justify-center py-5">
+            {
+                products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body name={product.name}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>)
+                )}
+
         </div>
     );
 }
